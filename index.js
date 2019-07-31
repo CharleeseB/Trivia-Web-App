403e1c47f2cf32fa8821cbb6e9c9ee4630248210
Charleese Burns <charleeseburns1@yahoo.com>
2019-07-31 13:21:18 -0400

    fixed js and css problem

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
 
 function countdownTimer(){
 let timeleft = 10;
-     let downloadTimer = setInterval(function(){
-       document.getElementById("countdown").innerHTML = timeleft + " seconds" ;
-       timeleft -= 1;
-       if(timeleft < 0){
-         clearInterval(downloadTimer);
-         document.getElementById("countdown").innerHTML = "Finished"
-       }
-     }, 1000);
+    let downloadTimer = setInterval(function(){
+      document.getElementById("countdown").innerHTML = timeleft + " seconds" ;
+      timeleft -= 1;
+      if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Finished"
+      }
+    }, 1000);
 }
 
 //on game start
 //firstQuestion();
 countdownTimer();
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log(`It's better to burn out than to fade away.`);
-//     const container = document.querySelector(".container");
-//     let new1 = userData.results.map(user => {
-//       return {
-//         name: `${user.name.first} ${user.name.last}`,
-//         location: `${user.location.street}</br>${user.location.city} ${
-//           user.location.state
-//         }</br> ${user.location.postcode}`,
-//         email: `${user.email}`,
-//         phone: `${user.cell}`,
-//         picture: `${user.picture.thumbnail}`
-//       };
-//     });
-//     const createDiv = new1 => {
-//       console.log(new1);
-//       const book = document.createElement("section");
-//       book.classList.add("avatar");
-//       container.appendChild(book);
-//       // console.log(user.name.first)
-//       book.innerHTML = `
-//               <section class="avatar">
-//               <div class="avatar-image">
-//                   <img src="${new1.picture}" alt="${new1.name} "/>
-//               </div>
-//               <div class="avatar-content">
-//                   <h2 class="avatar-header">${new1.name}</h2>
-//                   <div class="avatar-location">
-//                       ${new1.location}
-//                   </div>
-//                   <ul class="avatar-contact-list">
-//                       <li class="avatar-contact-list-item">
-//                           <a href="${new1.email}">✉</a>
-//                       </li>
-//                       <li class="avatar-contact-list-item">
-//                           <a href=${new1.cell}>✆</a> 
-//                       </li>
-//                   </ul>
-//               </div>
-//           </section>                     
-
-      
+const url = "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple";
       const getQuestion = () => {
         return fetch(url)
         .then(function(response) {
@@ -74,7 +32,7 @@ countdownTimer();
         question.answers.forEach((answer,index)=>{
           let id = "answer-"+ index;
           document.getElementById(id).innerHTML=answer;
-          console.log(answer, index)
+          console.log(answer, index);
         })
       }
       function updateQuestion(question){
@@ -105,6 +63,6 @@ countdownTimer();
           document.getElementById("answer-"+ i).addEventListener("click", clickHandler)
         }
       }
-      setup()
-      nextQuestion()
-    });
+      setup();
+      nextQuestion();
+    
