8d8f6ac7c511ccad5fa5bec6eb9ac2039b2d16e7
Armond Snowden <armond.z.snowden@gmail.com>
2019-08-01 13:11:53 -0400

    js for css 7/31

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+let correct = 0;
+let incorrect = 0;
+
+function start(){
+      console.log('you wanna play a game?');
+      $("hidden-container2").show();
+      $("header").hide();
+      countdownTimer();
+      document.getElementById("mainCard").innerHTML = "correct: " + correct;
+};
+// function startNew(){
+//   document.getElementById("mainCard").innerHTML = "correct: " + correct;
+//   countdownTimer();
+
+// }
+  
+    // setTimeout(function(){
+    // nextQuestion
+    // }, delay);
+    // startNew();}
+
+
+// function answerCounter(){
+//   let correct = 0;
+//   let incorrect = 0;
+  
+// }
+
 
 function countdownTimer(){
 let timeleft = 10;
@@ -6,14 +34,21 @@ let timeleft = 10;
       timeleft -= 1;
       if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Finished"
+        document.getElementById("countdown").innerHTML = "Moving On.."
       }
     }, 1000);
 }
 
+// function hideGame(){
+//   startGame.addEventListener("click",function(i){
+//     console.log("glakjfd")
+//     hideQ.style.display === "none";
+//     }
+//   });
+  
+
 //on game start
 //firstQuestion();
-countdownTimer();
 const url = "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple";
       const getQuestion = () => {
         return fetch(url)
@@ -50,12 +85,22 @@ const url = "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&
       });
       };
       function clickHandler(e){
+        let correct = 0;
+        let incorrect = 0;
         const answerSelected = e.srcElement.innerHTML;
         console.log(answerSelected);
         if (answerSelected === question.correct_answer){
           alert("correct")
+          correct += 1;
         }
-        console.log(question)
+        else{
+          incorrect += 1;
+        }
+        document.getElementById("mainCard").innerHTML = "correct: " + correct;
+
+        console.log(correct)
+        
+        
       }
 
       function setup(){
@@ -65,4 +110,5 @@ const url = "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&
       }
       setup();
       nextQuestion();
+    
     
