73bbef10d9d352ac9ae1c6f508e56ff220f5fa07
Charleese Burns <charleeseburns1@yahoo.com>
2019-08-02 14:12:58 -0400

    fixed timer,click boxes and bug fix

--- index.js
+++ index.js
@@ -1,169 +1,121 @@
 let correct = 0;
 let incorrect = 0;
 
-async function start(){
-      console.log('you wanna play a game?');
-      $("hidden-container2").show();
-      $("header").hide();
-      let question = await getQuestion();
-      countdownTimer();
-      setup();
-      updateButtons(question);
-      updateQuestion(question);
-      
-      document.getElementById("mainCard").innerHTML = "correct: " + correct;
-};
-// function startNew(){
-//   document.getElementById("mainCard").innerHTML = "correct: " + correct;
-//   countdownTimer();
+async function start() {
+  $("hidden-container2").show();
+  $("header").hide();
+  window.question = await getQuestion();
+  countdownTimer();
+  // setup();
+  updateButtons(window.question);
+  updateQuestion(window.question);
 
-// }
-  
-    // setTimeout(function(){
-    // nextQuestion
-    // }, delay);
-    // startNew();}
+  document.getElementById("mainCard").innerHTML = "correct: " + correct;
+}
 
-
-// function answerCounter(){
-//   let correct = 0;
-//   let incorrect = 0;
-  
-// }
-
-
-function countdownTimer(){
-let timeleft = 10;
-    let downloadTimer = setInterval(function(){
-      document.getElementById("countdown").innerHTML = timeleft + " seconds" ;
-      timeleft -= 1;
-      if(timeleft < 0){
-        clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Moving On.."
-      }
-    }, 1000);
+function countdownTimer() {
+  let timeleft = 60;
+  let downloadTimer = setInterval(function() {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds";
+    timeleft -= 1;
+    if (timeleft < 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").innerHTML = "Game Over!";
+    }
+  }, 1000);
 }
 const getSetup = () => {
   const jokeUrl = "https://official-joke-api.appspot.com/random_joke";
-    return fetch(jokeUrl)
-    .then(function(response){
+  return fetch(jokeUrl)
+    .then(function(response) {
       return response.json();
-    }).then(function(returnjson){
+    })
+    .then(function(returnjson) {
       let setup1 = returnjson.setup;
       let punchline = returnjson.punchline;
 
-
-
-      document.querySelector(".pic-box").innerHTML= setup1+ " " + punchline;
+      document.querySelector(".pic-box").innerHTML = setup1 + " " + punchline;
       localStorage.setItem("punchline", returnjson.punchline);
-      const jsonPunchline = () =>{localStorage.parse("punchline");
+      const jsonPunchline = () => {
+        localStorage.parse("punchline");
         const storedPunchline = localStorage.getItem("punchline");
         alert("storedPunchline");
-    }
-      console.log(setup1);
-      
-    })
-    
-  }
-  
-  getSetup();
+      };
+    });
+};
 
+getSetup();
 
+const url =
+  "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple";
 
-// function hideGame(){
-//   startGame.addEventListener("click",function(i){
-//     console.log("glakjfd")
-//     hideQ.style.display === "none";
-//     }
-//   });
-
-//on game start
-//firstQuestion();
-const url = "https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple";
-
-function getQuestion(){
+function getQuestion() {
   return fetch(url)
-  .then(function(response) {
-    return response.json();
-  }).then(function(returnjson){
-    let question1 = returnjson.results[0];
-    question1.incorrect_answers.push(question1.correct_answer);
-    question1.answers = question1.incorrect_answers;
-    delete question1.incorrect_answers;
-    console.log(question1);
-    return question1;
-  });
-};
-
-
-
-function updateButtons(question){
-  question.answers.forEach((answer,index)=>{
-    let id = "answer-"+ index;
-    document.getElementById(id).innerHTML=answer;
-    console.log(answer, index);
-  })
-}
-
-function updateQuestion(question){
-  document.getElementById("question").innerHTML= question.question;
-} 
-
-
-function randNum (low,hi){
-  return Math.floor(Math.random() * hi) + low; 
-};
-
-
-function nextQuestion(){
-  getQuestion().then(function(response){
-    const preAnswer = document.querySelectorAll(".answer");
-    preAnswer.forEach(box =>{
-      box.style.backgroundColor = "black";
+    .then(function(response) {
+      return response.json();
     })
-    question = response;
-    updateButtons(response)
-    updateQuestion(response)
-  });
-};
-
-
-function clickHandler(e){
-  let correct = 0;
-  let incorrect = 0;
-  const answerSelected = e.srcElement.innerHTML;
-  console.log(answerSelected);
-  if (answerSelected === question.correct_answer){
-    document.getElementById("answer-3").style.backgroundColor = "green";
-    alert("correct")
-    correct += 1;
-  }
-  else{
-    // document.getElementById("answer-0").style.backgroundColor = "red";
-    // document.getElementById("answer-1").style.backgroundColor = "red";
-    // document.getElementById("answer-2").style.backgroundColor = "red";
-    document.getElementById("answer-3").style.backgroundColor = "red";
-  }
-  document.getElementById("mainCard").innerHTML = "correct: " + correct;
-  console.log(correct)
-  
-  document.getElementById("mainCard").innerHTML = "correct: " + correct;
-
-  console.log(correct)
+    .then(function(returnjson) {
+      let question1 = returnjson.results[0];
+      question1.incorrect_answers.push(question1.correct_answer);
+      question1.answers = question1.incorrect_answers;
+      delete question1.incorrect_answers;
+      return question1;
+    });
 }
-function setup(){
-    for (let i = 0; i<4;i++){
-      document.getElementById("answer-"+ i).addEventListener("click", clickHandler)
-    }
+
+function updateButtons(question) {
+  question.answers.forEach((answer, index) => {
+    let id = "answer-" + index;
+    document.getElementById(id).innerHTML = answer;
+  });
+}
+document.getElementById("answer-3").style.order = "0";
+document.getElementById("answer-0").style.order = "1";
+document.getElementById("answer-1").style.order = "-1";
+document.getElementById("answer-2").style.order = "2";
+function updateQuestion(question) {
+  document.getElementById("question").innerHTML = window.question.question;
+}
+
+function nextQuestion() {
+  getQuestion().then(function(response) {
+    const preAnswer = document.querySelectorAll(".answer");
+    preAnswer.forEach(box => {
+      box.style.backgroundColor = "black";
+    });
+    question = response;
+    updateButtons(response);
+    updateQuestion(response);
+  });
+}
+
+function clickHandler(e) {
+  for (let correct = 0; correct < 1; correct++);
+  const answerSelected = e.srcElement.innerHTML;
+  if (answerSelected === window.question.correct_answer) {
+    document.getElementById("answer-3").style.backgroundColor = "green";
+    correct++;
+  } else {
+    document.getElementById("answer-0").style.backgroundColor = "red";
+    document.getElementById("answer-1").style.backgroundColor = "red";
+    document.getElementById("answer-2").style.backgroundColor = "red";
+    document.getElementById("answer-3").style.backgroundColor = "green";
   }
+  setTimeout(nextQuestion, 1000);
+  document.getElementById("mainCard").innerHTML = "correct: " + correct;
+  console.log(correct);
+}
+function setup() {
+  document
+    .querySelector(".question-container")
+    .addEventListener("click", clickHandler);
+}
 
+setup();
+nextQuestion();
 
-  setup();
-  nextQuestion();
+// const storedPunchline = localStorage.getItem("punchline");
 
-  const storedPunchline = localStorage.getItem("punchline");
-
-  function getPunchline (){
-    return jsonPunchline;
-  }
-
+// function getPunchline() {
+//   return jsonPunchline;
+// }
